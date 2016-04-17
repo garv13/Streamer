7588c6a107496d3f7b597c34f08310761097f930
garv13 <garvjasuja@outlook.com>
2016-04-17 23:01:46 +0530

    new commit

--- Heist/Login.xaml.cs
+++ Heist/Login.xaml.cs
@@ -5,6 +5,8 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text;
+using System.Threading.Tasks;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.Storage;
@@ -33,8 +35,21 @@ namespace Heist
             this.InitializeComponent();
         }
 
+        private async void Password_KeyDown(object sender, KeyRoutedEventArgs e)
+        {
+            if (e.Key == Windows.System.VirtualKey.Enter)
+            {
+                await lol();
+            }
+        }
+
         private async void Button_Click(object sender, RoutedEventArgs e)
         {
+            await lol();
+        }
+        private async Task lol()
+        { 
+            OfflineCheck oc = new OfflineCheck();
             LoadingBar.Visibility = Visibility.Visible;
             LoadingBar.IsIndeterminate = true;
             try
@@ -72,23 +87,42 @@ namespace Heist
             }
             catch (Exception)
             {
-                LoadingBar.Visibility = Visibility.Collapsed;
-                MessageDialog msgbox = new MessageDialog("Sorry Can't connect");
-                await msgbox.ShowAsync();
+                try
+                {
+                    OfflineCheck o;
+                    StorageFolder folder = Windows.Storage.ApplicationData.Current.LocalFolder;
+                    StorageFile sampleFile = await folder.GetFileAsync("check.txt");
+                    var t = await sampleFile.OpenAsync(FileAccessMode.Read);
+                    Stream na = t.AsStreamForRead();
+                    using (var streamReader = new StreamReader(na, Encoding.UTF8))
+                    {
+                        string line;
+                        line = streamReader.ReadToEnd();
+                        o = JsonConvert.DeserializeObject<OfflineCheck>(line);
+                        if ((UserName.Text.CompareTo(o.userName) == 0) && (Password.Password.CompareTo(o.password) == 0))
+                        {
+                            LoadingBar.Visibility = Visibility.Collapsed;
+                            MessageDialog msgbo = new MessageDialog("Welcome " + UserName.Text);
+                            await msgbo.ShowAsync();
+                            msgbo = new MessageDialog("You are not connected online hence you could only read the downloaded books..");
+                            await msgbo.ShowAsync();
+                            Frame.Navigate(typeof(Downloads));
+                        }
+                    }
+                }
+                catch (Exception)
+                {
+                    LoadingBar.Visibility = Visibility.Collapsed;
+                    MessageDialog msgbox = new MessageDialog("Sorry Can't connect");
+                    await msgbox.ShowAsync();
+                }
             }
-         }
-            
+        }
+
 
         private void Button_Click_1(object sender, RoutedEventArgs e)
         {
             Frame.Navigate(typeof(SignUp));
-        }
-        private async void Password_KeyDown(object sender, KeyRoutedEventArgs e)
-        {
-            if (e.Key == Windows.System.VirtualKey.Enter)
-            {
-                await lol();
-            }
         }
     }
 }
